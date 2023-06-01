cb759aae1ae44bc601f61fbfaafc45fe67370a24
Payton Gardner <payton.gardner@umontana.edu>
2023-06-01 14:20:51 +0200

    Added alpha,n U,Th reactions.

--- He_tools.py
+++ He_tools.py
@@ -7,10 +7,7 @@ Created on Thu Mar 03 12:16:15 2011
 @author: gar279
 """
 
-from noble_gas_tools import *
-from numpy import *
-#from freesteam import *
-import string
+import numpy as np
 import pdb
 
 
@@ -25,6 +22,7 @@ class rock_type:
         self.density = density
         self.HPR = self.He_prod_rate()
         self.APR = self.Ar_prod_rate()
+        self.NFlux = 0
 
     class He_prod_rate:
         '''the production rate for a given rock composition of 3he 4he in given units'''
@@ -37,7 +35,7 @@ class rock_type:
         '''The radiogenic production rate for a given rock compoosition.'''
         def __init__(self):
             self.Ar40_value = 0
-            self.Ar39_value = 0
+            self.Ar39_U_Th_value = 0
             self.Ar40_accum = 0
             self.units = 'atoms/g_rock/yr'
 
@@ -101,8 +99,8 @@ class rock_type:
                    a = 'mol' 
                 
             else:
-                calcHe_prod_rate_whole_rock();
-                calcAr_prod_rate_whole_rock()
+                self.HPR.calcHe_prod_rate_whole_rock();
+                self.APR.calcAr_prod_rate_whole_rock()
                 print('not implemented yet, converted back to atoms.')
                 a = 'atoms';
             
@@ -149,8 +147,8 @@ class rock_type:
                     b = 'kg_h2o';
                 
             else:
-                calcHe_prod_rate_whole_rock();
-                calcAr_prod_rate_whole_rock()
+                self.HPR.calcHe_prod_rate_whole_rock();
+                self.APR.calcAr_prod_rate_whole_rock()
                 print('not implemented yet no conversion converted back g_rock. try again now')
                 b = 'g_rock'
             
@@ -163,8 +161,8 @@ class rock_type:
                     self.APR.Ar40_value = self.APR.Ar40_value/yr_to_s;
                     c = 's';
             else:
-                calcHe_prod_rate_whole_rock();
-                calcAr_prod_rate_whole_rock()
+                self.HPR.calcHe_prod_rate_whole_rock();
+                self.APR.calcAr_prod_rate_whole_rock()
                 print('not implemented yet no conversion converted back yr. try again now')
                 c = 'yr';
                 
@@ -182,8 +180,10 @@ class rock_type:
             #composition mass fractions
             comp_dict = {'Li':2.0e-5,'U':2.8e-6,'Na':2.89e-2,\
                             'Mg':1.33e-2,'Al':8.04e-2,'Si':3.09e-1,\
-                            'C':3.24e-3,'Th':1.07e-5,'K':2.82e-2} #Ballanine and Burnard Reviews in Geochem, pg 488
-            rli_capt_prob = 2.05e-4;  #at some point this could be calculated from the composition
+                            'C':3.24e-3,'Th':1.07e-5,'K':2.82e-2,\
+                            'O':4.75e-1,'Fl':5.57e-4,'Ca':3e-2, \
+                            'Ti':3e-3,'Fe':3.15e-2} #Ballanine and Burnard Reviews in Geochem, pg 488, flourine taken from Sramek 2017
+            rli_capt_prob = 2.05e-4;  #Hardcoded from Ballentine - at some point this should be calculated from the composition
             rtotal_capt_prob = 9.79e-3;
             rdensity = 2.7 # g/cc
             rporosity = 0.1; 
@@ -194,6 +194,81 @@ class rock_type:
         self.total_capt_prob = rtotal_capt_prob
         self.porosity = rporosity
         self.density = rdensity
+    
+    def calc_neutron_flux(self):
+        '''Code taken from publication:
+            
+                O. Šrámek, L. Stevens, W. F. McDonough, S. Mukhopadhyay, and 
+                J. Peterson: “Subterranean production of neutrons, 39Ar and 
+                21Ne: Rates and uncertainties.” Submitted to Geochim. Cosmochim. Acta
+                (arXiv:1509.07436).
+            
+                Ondřej Šrámek, Charles Univ Prague, ondrej.sramek@gmail.com, Sep 2015
+        '''
+        wtfrac = np.array([
+            self.composition['C'],  #6 carbon
+            self.composition['O'],    #8 oxygen
+            self.composition['Fl'],  #9 fluorine
+            self.composition['Na'],   #11 sodium
+            self.composition['Mg'],   #12 magnesium
+            self.composition['Al'],   #13 aluminum
+            self.composition['Si'],    #14 silicon
+            self.composition['K'],   #19 potassium
+            self.composition['Ca'],   #20 calcium
+            self.composition['Ti'],  #22 titanium
+            self.composition['Fe'],   #26 iron
+            self.composition['Th'],  #90 thorium
+            self.composition['U']    #92 uranium
+            ])
+        
+        el = np.zeros(15) #array to multiply with elem abund of target nuclies
+        el[0] = wtfrac[5] #Al
+        el[1] = wtfrac[3] #Na
+        el[2] = wtfrac[6] #Si
+        el[3] = wtfrac[6] #Si
+        el[4] = wtfrac[1] #O
+        el[5] = wtfrac[4] #Mg
+        el[6] = wtfrac[4] #Mg
+        el[7] = wtfrac[2] #F
+        el[8] = wtfrac[1] #O
+        el[9] = wtfrac[10] #Fe
+        el[10] = wtfrac[7] #K
+        el[11] = wtfrac[9] #Ti
+        el[12] = wtfrac[0] #C
+        el[13] = wtfrac[8] #Ca
+        el[14] =  1. #SF
+        
+        pp = np.zeros(3) #array to multiply with elem abund of chain parents
+        pp[0] = wtfrac[11] #Th
+        pp[1] = wtfrac[12] #U
+        pp[2] = wtfrac[12] #U
+        
+        cn = np.array([
+            [2.65E+09, 3.31E+08, 5.03E+09],
+            [6.07E+09, 8.02E+08, 1.23E+10],
+            [1.95E+08, 2.53E+07, 3.91E+08],
+            [1.68E+08, 2.05E+07, 3.17E+08],
+            [8.77E+07, 1.33E+07, 2.27E+08],
+            [1.72E+09, 2.41E+08, 3.72E+09],
+            [1.01E+09, 1.44E+08, 2.22E+09],
+            [1.60E+10, 2.34E+09, 3.75E+10],
+            [9.50E+06, 1.43E+06, 2.47E+07],
+            [1.28E+08, 3.35E+06, 9.55E+07],
+            [1.10E+08, 8.92E+06, 1.64E+08],
+            [4.35E+08, 2.20E+07, 5.00E+08],
+            [3.61E+08, 5.48E+07, 9.96E+08],
+            [2.98E+07, 2.22E+06, 4.29E+07],
+            [3.01E+03, 2.37E+03, 4.44E+08]
+        ])
+
+        nr = np.zeros(cn.shape)
+        
+        for i in range(cn.shape[0]):
+            for j in range(cn.shape[1]):
+                nr[i,j] = cn[i,j] * el[i] * pp[j]
+        
+        Sn = nr.sum() #neutrons/kg_rock/yr
+        self.NFlux=Sn/1000. #neutrons/g_rock/yr
 
     def calcHe_prod_rate_whole_rock(self):
         '''Calculate the He_prod_rate for the rock type '''
@@ -220,9 +295,84 @@ class rock_type:
         Ma = 39.964
         Na = 6.023e23
         Xk = 1.176e-4
-        F = Xk*Na/Ma*lamma_e/lamma_k*(exp(lamma_k*1)-1)
+        F = Xk*Na/Ma*lamma_e/lamma_k*(np.exp(lamma_k*1)-1)
         self.APR.Ar40_value = self.composition['K']*F 
         self.APR.units ='atoms/g_rock/yr'
+    
+    def calc39Ar_U_Th_prod(self):
+        '''Calculates the 39Ar produces from fissiongenic neutrons in the subsurface. 
+           Uses code from:
+            O. Šrámek, L. Stevens, W. F. McDonough, S. Mukhopadhyay, and 
+            J. Peterson: “Subterranean production of neutrons, 39Ar and 
+            21Ne: Rates and uncertainties.” Submitted to Geochim. Cosmochim. Acta
+            (arXiv:1509.07436).
+            
+            Ondřej Šrámek, Charles Univ Prague, ondrej.sramek@gmail.com, Sep 2015 '''
+        wtfrac = np.array([
+            self.composition['C'],  #6 carbon
+            self.composition['O'],    #8 oxygen
+            self.composition['Fl'],  #9 fluorine
+            self.composition['Na'],   #11 sodium
+            self.composition['Mg'],   #12 magnesium
+            self.composition['Al'],   #13 aluminum
+            self.composition['Si'],    #14 silicon
+            self.composition['K'],   #19 potassium
+            self.composition['Ca'],   #20 calcium
+            self.composition['Ti'],  #22 titanium
+            self.composition['Fe'],   #26 iron
+            self.composition['Th'],  #90 thorium
+            self.composition['U']    #92 uranium
+        ])
+        
+        el = np.zeros(15) #array to multiply with elem abund of target nuclies
+        el[0] = wtfrac[5] #Al
+        el[1] = wtfrac[3] #Na
+        el[2] = wtfrac[6] #Si
+        el[3] = wtfrac[6] #Si
+        el[4] = wtfrac[1] #O
+        el[5] = wtfrac[4] #Mg
+        el[6] = wtfrac[4] #Mg
+        el[7] = wtfrac[2] #F
+        el[8] = wtfrac[1] #O
+        el[9] = wtfrac[10] #Fe
+        el[10] = wtfrac[7] #K
+        el[11] = wtfrac[9] #Ti
+        el[12] = wtfrac[0] #C
+        el[13] = wtfrac[8] #Ca
+        el[14] =  1. #SF
+        
+        pp = np.zeros(3) #array to multiply with elem abund of chain parents
+        pp[0] = wtfrac[11] #Th
+        pp[1] = wtfrac[12] #U
+        pp[2] = wtfrac[12] #U
+        
+        ca = np.array([
+            [2.59E+08, 2.36E+07, 3.92E+08],
+            [5.04E+08, 4.46E+07, 8.02E+08],
+            [3.19E+07, 4.05E+06, 6.46E+07],
+            [1.37E+07, 1.35E+06, 2.51E+07],
+            [1.97E+07, 2.89E+06, 4.68E+07],
+            [3.92E+08, 5.43E+07, 8.13E+08],
+            [2.75E+08, 4.04E+07, 6.33E+08],
+            [1.78E+09, 2.06E+08, 3.17E+09],
+            [2.34E+06, 3.39E+05, 5.70E+06],
+            [6.81E+06, 2.53E+04, 1.62E+06],
+            [1.06E+07, 4.72E+05, 1.22E+07],
+            [5.88E+07, 2.29E+06, 5.89E+07],
+            [1.31E+08, 2.10E+07, 4.02E+08],
+            [3.96E+06, 2.13E+05, 4.80E+06],
+            [2.88E+02, 3.09E+02, 4.73E+07]
+        ])
+
+        ar = np.zeros(ca.shape)
+        
+        for i in range(ca.shape[0]):
+            for j in range(ca.shape[1]):
+                ar[i,j] = ca[i,j] * el[i] * pp[j] * el[10]
+        
+        S_Ar = ar.sum() #atoms/kg_rock/yr
+        self.APR.Ar39_U_Th_value=S_Ar/1000. #atoms/g_rock/yr
+        
 
     def calcAr40_accum(self,age):
         '''calculate the accumulated 40Ar for a given rock for a given age in years.  
@@ -231,11 +381,10 @@ class rock_type:
             print('accumulation only works for age in years')
         lamma_e = 0.581e-10 #1/yr
         lamma_k = 5.463e-10 #1/yr
-        lamma = lamma_e+lamma_k
         Ma = 39.964
         Na = 6.023e23
         Xk = 1.176e-4
-        F = Xk*Na/Ma*lamma_e/lamma_k*(exp(lamma_k*age)-1)
+        F = Xk*Na/Ma*lamma_e/lamma_k*(np.exp(lamma_k*age)-1)
         self.APR.Ar40_accum = self.composition['K']*F 
 
 def calcHe_prod_rate_refresh(rock_type):
